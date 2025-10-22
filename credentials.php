4009aa3bca088ce7357f5a797bcc05c857c4c6d5
Leng-TT <lengthao551@gmail.com>
2025-10-22 19:22:19 +0000

    First commit

---
+++ credentials.php
@@ -0,0 +1,8 @@
+//Remember to popualate :P
+//
+<?php
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
+$dbname = 'employees';              //which db you're going to use
+$username = 'phpuser1';
+$password = 'abc123';
+?>
